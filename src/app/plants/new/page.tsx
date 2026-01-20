2147a12dd1624829fa65849f2c1d2932c1ed381a
Claire Kwong <clairebkwong@gmail.com>
2026-01-19 22:48:39 -0500

    feat: create plant form 2

---
+++ src/app/plants/new/page.tsx
@@ -0,0 +1,47 @@
+import Form from "next/form";
+import prisma from "@/../lib/prisma";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export default function NewPost() {
+  async function createPlant(formData: FormData) {
+    "use server";
+
+    const name = formData.get("name") as string;
+
+    await prisma.plant.create({
+      data: {
+        name,
+      },
+    });
+
+    revalidatePath("/plants");
+    redirect("/plants");
+  }
+
+  return (
+    <div className="max-w-2xl mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-6">Create New Plant</h1>
+      <Form action={createPlant} className="space-y-6">
+        <div>
+          <label htmlFor="title" className="block text-lg mb-2">
+            Title
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter your plant name"
+            className="w-full px-4 py-2 border rounded-lg"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600"
+        >
+          Create Plant
+        </button>
+      </Form>
+    </div>
+  );
+}
