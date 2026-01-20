336630b9d46fbbcd9bf21b52ae4b8b743b386c10
Claire Kwong <clairebkwong@gmail.com>
2026-01-20 12:40:12 -0500

    feat: optionally set date string

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@ export default async function Home() {
     "use server";
 
     const plantId = Number(formData.get("plantId"));
+    const dateString = formData.get("date") as string;
 
     await prisma.event.create({
       data: {
         plantId,
+        ...(dateString && { date: new Date(dateString) }),
       },
     });
 
@@ -28,10 +30,14 @@ export default async function Home() {
         {plants.map((plant) => (
           <li key={plant.id} className="mb-2">
             <form action={waterPlant}>
-              <Link href={{ pathname: "/plants/" + plant.id }}>
+              <Link
+                href={{ pathname: "/plants/" + plant.id }}
+                className="font-semibold m-4"
+              >
                 {plant.name}
               </Link>
               <input type="hidden" name="plantId" value={plant.id}></input>
+              <input type="date" name="date" className="m-4" />
               <button
                 type="submit"
                 className="m-4 px-4 py-2 bg-green-600 text-white font-semibold rounded hover:bg-green-700 transition duration-150"
