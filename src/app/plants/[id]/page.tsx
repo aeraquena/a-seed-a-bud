16ef1020f3d544440f150bc6ab9b1de048735c06
Claire Kwong <clairebkwong@gmail.com>
2026-01-19 22:48:34 -0500

    feat: create plant form

--- src/app/plants/[id]/page.tsx
+++ src/app/plants/[id]/page.tsx
@@ -19,6 +19,8 @@ export default async function Post({
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
       <article className="max-w-2xl space-y-4 font-[family-name:var(--font-geist-sans)]">
         <h1 className="text-4xl font-bold mb-8 text-[#333333]">{plant.name}</h1>
+        <p>Created at {new Date(plant.createdAt).toString()}</p>
+        <p>Updated at {new Date(plant.updatedAt).toString()}</p>
       </article>
     </div>
   );
