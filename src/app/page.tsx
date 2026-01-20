6a594a5ef4716f2d0ab46685262f451e04b52ab9
Claire Kwong <clairebkwong@gmail.com>
2026-01-20 15:52:44 -0500

    fix: padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default async function Home() {
   });
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center mt-8">
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center pt-8">
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
         A Seed! A Bud!
       </h1>
