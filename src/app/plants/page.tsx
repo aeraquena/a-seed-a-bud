250beaddd6dd690b24d3bb83bdfdee024df2442b
Claire Kwong <clairebkwong@gmail.com>
2026-01-20 11:31:42 -0500

    fix: remove redirect

--- src/app/plants/page.tsx
+++ src/app/plants/page.tsx
@@ -1,6 +1,9 @@
-//import prisma from "@/../lib/prisma";
-import { redirect } from "next/navigation";
-
 export default async function Home() {
-  redirect("/");
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Plants
+      </h1>
+    </div>
+  );
 }
